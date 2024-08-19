82f60bf70aa4263590c8ff28510edc918dcb8761
ozzgurkarli <ozzgurkarli2@gmail.com>
2024-08-19 12:44:59 +0300

    migration

---
+++ BankingApp.Entity/Migrations/20240819094444_ColumnsAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingApp.Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Primary",
+                table: "MailAddress",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Temporary",
+                table: "Login",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "Credit",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Primary",
+                table: "MailAddress");
+
+            migrationBuilder.DropColumn(
+                name: "Temporary",
+                table: "Login");
+
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "Credit");
+        }
+    }
+}
